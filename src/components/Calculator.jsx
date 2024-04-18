738fe72c382cf855f1b6a6cdb16fbbebe66758f3
abh-i-navv <abhinav11290@gmail.com>
2024-04-18 17:42:59 +0530

    ui changes and updates

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Calculator() {
 
@@ -37,40 +37,58 @@ export default function Calculator() {
         const pip = Math.abs(data.open-data.close)
         const loss = (data.account*data.risk)/100
         const contract = data.contract
-
+        
         let lot = loss/(pip*contract)
+        lot = Math.round(lot*100000)/100000
         setLot(lot)
     }
 
+    const onKeyDown = (e) => {
+        if(e.key === "Enter"){
+            return lotCalc()
+        }
+    }
+
+    useEffect(()=>{
+
+        document.addEventListener('keydown',onKeyDown)
+
+        return () => {
+            document.removeEventListener('keydown', onKeyDown)
+        }
+
+    },[])
+
+
   return (
-    <div className='sm:w-[500px] h-auto m-auto relative border-2 bg-zinc-900 rounded-3xl border-[#6c35de] p-5'>
+    <div className='sm:w-[600px] h-auto m-auto relative border-2 bg-zinc-900 rounded-3xl border-[#6d35deab] p-5 text-xl'>
         <div className='grid grid-cols-1 sm:grid-cols-2 sm:m-2'>
             <div className='h-auto w-auto m-3 sm:m-5'>
-                <label htmlFor='opening' className='py=3'>Opening Price</label>
-                <input id='opening' placeholder='opening price' className='w-full h-10 mt-2 p-3 text-[#ffffff] placeholder:text-[#ffffff] bg-zinc-800 border border-[#6c35de] rounded-lg' onChange={handleOnChange}></input>
+                <label htmlFor='opening'>Opening Price</label>
+                <input id='opening' placeholder='opening price' className='w-full h-10 shadow-sm focus:outline-none focus:shadow-white shadow-[#6c35de] mt-2 p-3 text-[#ffffff] placeholder:text-[#999999] placeholder:text-lg bg-zinc-800 border border-[#6c35de] rounded-lg' onChange={handleOnChange}></input>
             </div>
             <div className='h-auto w-auto m-3 sm:m-5'>
                 <label htmlFor='closing'>Stop Price</label>
-                <input id='closing' placeholder='closing price' className='w-full h-10 mt-2 p-3 text-[#ffffff] placeholder:text-[#ffffff] bg-zinc-800 border border-[#6c35de] rounded-lg' onChange={handleOnChange}></input>
+                <input id='closing' placeholder='stop price' className='w-full h-10 mt-2 p-3 shadow-sm focus:outline-none focus:shadow-white shadow-[#6c35de] text-[#ffffff] placeholder:text-[#999999] bg-zinc-800 border border-[#6c35de] rounded-lg' onChange={handleOnChange}></input>
             </div>
             <div className='h-auto w-auto m-3 sm:m-5'>
                 <label htmlFor='size'>Account Size</label>
-                <input id='size' placeholder='account size' className='w-full h-10 mt-2 p-3 text-[#ffffff] placeholder:text-[#ffffff] bg-zinc-800 border border-[#6c35de] rounded-lg' onChange={handleOnChange}></input>
+                <input id='size' placeholder='account size' className='w-full h-10 mt-2 p-3 shadow-sm focus:outline-none focus:shadow-white shadow-[#6c35de] text-[#ffffff] placeholder:text-[#999999] bg-zinc-800 border border-[#6c35de] rounded-lg' onChange={handleOnChange}></input>
             </div>
 
             <div className='h-auto w-auto m-3 sm:m-5'>
                 <label htmlFor='risk'>Risk %</label><br></br>
-                <input id='risk' placeholder='risk percentage' className='w-full h-10 mt-2 p-3 text-[#ffffff] placeholder:text-[#ffffff] bg-zinc-800 border border-[#6c35de] rounded-lg' onChange={handleOnChange}></input>
+                <input id='risk' placeholder='risk percentage' className='w-full h-10 mt-2 p-3 shadow-sm focus:outline-none focus:shadow-white shadow-[#6c35de] text-[#ffffff] placeholder:text-[#999999] bg-zinc-800 border border-[#6c35de] rounded-lg' onChange={handleOnChange}></input>
             </div>
             <div className='h-auto w-auto m-3 sm:m-5'>
                 <label htmlFor='contract'>Contract Size</label><br></br>
-                <input id='contract' placeholder='contract size' className='w-full h-10 mt-2 p-3 text-[#ffffff] placeholder:text-[#ffffff] bg-zinc-800 border border-[#6c35de] rounded-lg' onChange={handleOnChange}></input>
+                <input id='contract' placeholder='contract size' className='w-full h-10 mt-2 p-3 shadow-sm focus:outline-none focus:shadow-white shadow-[#6c35de] text-[#ffffff] placeholder:text-[#999999] bg-zinc-800 border border-[#6c35de] rounded-lg' onChange={handleOnChange}></input>
             </div>
         </div>
         <div className='flex justify-center items-center m-5'>
-            <button onClick={lotCalc} className='border border-[#6c35de] hover:bg-zinc-950 rounded-xl md:w-[150px] p-2 '>Calculate</button>
+            <button onClick={lotCalc} className='border border-[#6c35de] bg-zinc-800 hover:opacity-100 opacity-80 hover:shadow-sm hover:shadow-white rounded-xl sm:w-[150px] p-2 '>Calculate</button>
         </div>
-        <span className='flex justify-center items-center m-5 text-xl'>
+        <span className='flex justify-center items-center m-3 sm:m-5 text-2xl'>
         {lot ? lot: <></>}
         </span>
     
